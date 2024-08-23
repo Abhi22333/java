db9c579f42a4cbe7859335908de434ab2d771c11
Abhi22333 <abhishektc.xworkz@gmail.com>
2024-08-23 08:07:34 +0530

    array

---
+++ java/Return.java
@@ -0,0 +1,20 @@
+class Return{
+	
+	public static String run(int age)
+	{
+	for(int i=1;i>18;i++)
+	{
+	if (i>18){
+		
+    return "can vote";
+		}
+	
+	else{
+	return"cant vote";
+	
+	}
+	
+	
+}
+	}
+}
